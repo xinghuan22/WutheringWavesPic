f72e1dd4b6b6cff92d783281155db313989ddf9a
xinghuan22 <xinghuan22@users.noreply.github.com>
2025-06-14 09:45:16 +0000

    deploy: 6d451631cf13f97886e8193555620c082215d8a2

--- js/render.js
+++ js/render.js
@@ -3,7 +3,7 @@ const folder = params.get("folder");
 document.getElementById("title").innerText = `📁 ${folder}`;
 const gallery = document.getElementById("gallery");
 
-const base = `https://raw.githubusercontent.com/xinghuan22/WutheringWavesPic/main/${folder}/`;
+const base = `https://raw.githubusercontent.com/${location.hostname.split('.')[0]}/${location.pathname.split('/')[1]}/main/${folder}/`;
 
 fetch(`${base}index.json`)
   .then(r => r.json())
@@ -12,6 +12,7 @@ fetch(`${base}index.json`)
       const el = document.createElement("img");
       el.src = base + img;
       el.alt = img;
+      el.style = "max-width:200px;margin:10px;border:1px solid #ccc;";
       gallery.appendChild(el);
     });
   });
